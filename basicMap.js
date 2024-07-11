d3a94d8cfa67c46d70d37b06dbf44163099183dc
jacobeshete <jacobeshete@icloud.com>
2024-07-11 16:52:02 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,9 +1,9 @@
-console.log("basicMap.js loaded.");
+console.log("basic.js loaded.");
 
-// CPCC Coordinates: 35.2176665 N, 80.831473 W -> 35.2176665, -80.831473
+// CPCC Coordinates: 35.2176665 North, 80.831473 West -> 35.2176665, -80.831473
 
-let map = L.map("map", {
-  center: [32.2176665, -80.831473],
+let map = L.map("map",{
+  center: [35.2176665, -80.831473],
   zoom: 16
 });
 
