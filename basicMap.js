bb600fcf32a268c12beacb3623c42be79d9b8e36
jacobeshete <jacobeshete@icloud.com>
2024-07-16 16:05:57 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -14,4 +14,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 L.marker([35.2176665, -80.831473], {
   title: "Hello from CPCC!"
-}).addTo(map);
+}).bindPopup("hello From CPCC!")
+  .addTo(map);
